648924bf3cfd78335c239085bde22f42ac0f3bde
Patrick White <pat.k.white@gmail.com>
2024-03-27 14:51:08 -0500

    feat: add hashing password encryption to register user mutation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,18 +1,41 @@
-import User from '../../models/User.js'
+import User from "../../models/User.js"
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import {} from 'dotenv'
 
 const usersResolvers = {
     Mutation: {
-        register: async (_, { registerInput: { email, password, confirmPassword, username }}, ) => {
+        register: async (
+            _,
+            { registerInput: { email, password, confirmPassword, username } }
+        ) => {
+            password = await bcrypt.hash(password, 12)
+
             const user = new User({
                 email,
                 password,
                 username,
-                createdAt: new Date()
-            })
-            await user.save()
-            return user
-        }
-    }
-}
+                createdAt: new Date(),
+            });
+            
+            const res = await user.save();
 
-export default usersResolvers
+            const SECRET_KEY = process.env.SECRET_KEY
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn: '1h' })
+
+            return {
+                ...res._doc,
+                id: res._id,
+                password,
+                token
+            };
+        },
+    },
+};
+
+export default usersResolvers;
