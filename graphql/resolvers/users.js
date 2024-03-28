b93e473d7daba7e119234ff9c1d368a390ef5d8f
Patrick White <pat.k.white@gmail.com>
2024-03-28 13:54:11 -0500

    fix(resolvers/users/register): validate username is unique

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,41 +1,56 @@
-import User from "../../models/User.js"
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import {} from 'dotenv'
+import User from "../../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {} from "dotenv";
+import { GraphQLError } from "graphql";
 
 const usersResolvers = {
-    Mutation: {
-        register: async (
-            _,
-            { registerInput: { email, password, confirmPassword, username } }
-        ) => {
-            password = await bcrypt.hash(password, 12)
+  Mutation: {
+    register: async (
+      _,
+      { registerInput: { email, password, confirmPassword, username } }
+    ) => {
+      password = await bcrypt.hash(password, 12);
 
-            const user = new User({
-                email,
-                password,
-                username,
-                createdAt: new Date(),
-            });
-            
-            const res = await user.save();
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        throw new GraphQLError("Username already exists.", {
+          extensions: {
+            code: "USERNAME_ALREADY_EXISTS",
+            argumentName: "username",
+          },
+        });
+      }
 
-            const SECRET_KEY = process.env.SECRET_KEY
+      const user = new User({
+        email,
+        password,
+        username,
+        createdAt: new Date(),
+      });
 
-            const token = jwt.sign({
-                id: res.id,
-                email: res.email,
-                username: res.username
-            }, SECRET_KEY, { expiresIn: '1h' })
+      const res = await user.save();
 
-            return {
-                ...res._doc,
-                id: res._id,
-                password,
-                token
-            };
+      const SECRET_KEY = process.env.SECRET_KEY;
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
         },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        password,
+        token,
+      };
     },
+  },
 };
 
 export default usersResolvers;
